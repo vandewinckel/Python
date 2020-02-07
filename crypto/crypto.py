68c28ae9c597500b4c39701ec76203d41a673bd2
vandewinckel <vandewinckels@gmail.com>
2020-02-07 04:02:31 +0000

    File Loader

--- crypto/crypto.py
+++ crypto/crypto.py
@@ -1,1 +1,11 @@
-print("Hello World")
+#init
+bits = []
+
+#File Load Function
+def load(file):
+    handle = open(file)
+    for bit in handle:
+        bits.append(bit)
+
+load('project2part2ciphertext.txt')
+print(bits)
