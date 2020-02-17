b2886ae9112a9327788043f6ae0769a70026e105
vandewinckel <vandewinckels@gmail.com>
2020-02-17 22:52:14 +0000

    init

--- crypto/IOC/main.py
+++ crypto/IOC/main.py
@@ -1,7 +1,7 @@
 # INIT
+#alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 data = []
-numeric = []
-normal = []
+freq = {}
 
 # File - Load Function
 def load(file):
@@ -11,6 +11,7 @@ def load(file):
 # Text Format
 def form(file):
     format = load(file)
+    format = ''.join([i for i in format if not i.isdigit()])
     format = format.replace(' ', '')
     format = format.replace(',', '')
     format = format.replace('-', '')
@@ -19,21 +20,29 @@ def form(file):
     format = format.replace('.', '')
     format = format.replace(';', '')
     format = format.replace('\n', '')
+    format = format.replace('(','')
+    format = format.replace(')','')
+    format = format.replace('"','')
+    format = format.replace('?','')
+    format = format.replace("'",'')
+    format = format.replace(':','')
+    format = format.upper()
     return format
 
-    #ADDS TO LIST
-    for letter in format:
-        data.append(letter)
+def FREQ(message,list_):
+    for i in message:
+        list_[i]=list_.get(i,0)+1
+    for i in sorted(list_):
+        print((i, list_[i]), end =" ")
 
-    #REMOVE NUM
-    for letter in data:
-        global numbers
-        numbers = ""
-        
-        if not letter.isdigit():
-            normal.append(letter)
-        else:
-            numeric.append(letter)
-            numbers = ''.join(numeric)
-            
-    return format, numbers
+def COUNTER(message,list_):
+    for letters in message:
+        list_.append(letters)
+    length = (len(list_))
+    return length
+
+form('File1.txt')
+message = form(input('File: '))
+
+print(COUNTER(message,data))
+FREQ(message,data)
