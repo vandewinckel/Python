8e140c60a63603a9075a71570b330024438cccf1
vandewinckel <vandewinckels@gmail.com>
2020-02-17 22:09:57 +0000

    init

--- crypto/IOC/main.py
+++ crypto/IOC/main.py
@@ -1,1 +1,39 @@
+# INIT
+data = []
+numeric = []
+normal = []
 
+# File - Load Function
+def load(file):
+    handle = open(file)
+    return handle.read()
+
+# Text Format
+def form(file):
+    format = load(file)
+    format = format.replace(' ', '')
+    format = format.replace(',', '')
+    format = format.replace('-', '')
+    format = format.replace('–', '')
+    format = format.replace('—', '')
+    format = format.replace('.', '')
+    format = format.replace(';', '')
+    format = format.replace('\n', '')
+    return format
+
+    #ADDS TO LIST
+    for letter in format:
+        data.append(letter)
+
+    #REMOVE NUM
+    for letter in data:
+        global numbers
+        numbers = ""
+        
+        if not letter.isdigit():
+            normal.append(letter)
+        else:
+            numeric.append(letter)
+            numbers = ''.join(numeric)
+            
+    return format, numbers
